f226c68e51329793fc2b27495f5c620c89320bf4
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-12-20 15:50:34 -0500

    all of my answers are incorrect, another issue to find

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,46 +17,47 @@ var highscoreDisplayScore = document.getElementById("highscore-score");
 
 var quizQuestions = [{
     title: "How many counties in NC?",
-    choices: [ "47", "67", "100", "39"],
-    answerIndex: 3
+    choices: ["47", "67", "100", "39"],
+    answerIndex: 2
 },
   {
     title: "Most populated county in NC?",
     choices: ["Anson", "Mecklenburg", "Wake", "Buncombe"],
-    answerIndex: 3
+    answerIndex: 2
 },
    {
     title: "Largest NC county by area?",
     choices: ["Davidson", "Sampson", "Dare", "Columbus"],
-    answerIndex: 3
+    answerIndex: [2]
 },
     {
     title: "NC County with the highest elevation?",
     choices: ["Burke", "Yancey", "Mitchell", "Wilkes"],
-    answerIndex: 3
+    answerIndex: [2]
 },
     {
     title: "Smallest NC county by area?",
     choices: ["Brunswick", "Carteret", "Hyde", "Mecklenburg"],
-    answerIndex: 3},  
+    answerIndex: [2]
+},  
     {
     title: "City of Charlotte is in which NC county?",
     choices: ["Gaston", "Iredell", "Mecklenburg", "Union"],
-    answerIndex: 3
+    answerIndex: [2]
 },
     {
     title: "Which NC county is the capital in?",
     choices: ["Durham", "Mecklenburg", "Wake", "Catawba"],
-    answerIndex: 3
+    answerIndex: 2
 },
          
     ];
 
 // Buttons
-var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
+var buttonA = document.getElementById(0);
+var buttonB = document.getElementById(1);
+var buttonC = document.getElementById(2);
+var buttonD = document.getElementById(3);
 
 
 
